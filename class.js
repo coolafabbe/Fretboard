1c8e28fbaaf28ee5087007befb49c052a3ab7822
Fasth <fabian.fasth@afconsult.com>
2020-12-11 20:42:53 +0100

    2nd commit

--- class.js
+++ class.js
@@ -19,24 +19,3 @@ class Choord {
         return this.name;
     }
 }
-
-allChoords = [  new Choord("major", ["P1", "M3", "P5"]),
-                new Choord("minor", ["P1", "m3", "P5"]),	
-                new Choord("aug",   ["P1", "M3", "m6"]),
-                new Choord("sus4",  ["P1", "P4", "P5"]),
-                new Choord("sus2",  ["P1", "M2", "P5"]),
-                new Choord("maj7",  ["P1", "M3", "P5", "M7"]),
-                new Choord("7",     ["P1", "M3", "P5", "m7"]),
-                new Choord("m7",    ["P1", "m3", "P5", "m7"]),
-                new Choord("7b5",   ["P1", "m3", "d5", "m7"]),
-                new Choord("dim",   ["P1", "m3", "d5", "M6"]),
-                new Choord("6",     ["P1", "M3", "P5", "M6"]),
-                new Choord("m6",    ["P1", "m3", "P5", "M6"]),
-                new Choord("add9",  ["P1", "M2", "M3", "P5"]),
-                new Choord("9",     ["P1", "M2", "M3", "P5", "m7"]),
-                new Choord("maj9",  ["P1", "M2", "M3", "P5", "M7"]),
-                new Choord("m9",    ["P1", "M2", "m3", "P5", "m7"]),
-                new Choord("6/9",   ["P1", "M2", "M3", "P5", "M6"]),
-                new Choord("11",    ["P1", "M2", "M3", "P4", "P5", "m7"]),
-                new Choord("m11",   ["P1", "M2", "m3", "P4", "P5", "m7"]),
-                new Choord("13",    ["P1", "M2", "M3",, "P5", "M6", "m7"])];
--- fretboard.js
+++ fretboard.js
@@ -1,103 +1,102 @@
+
 //Define notes
+const numOfFrets = 23;
 const allNotes = ["A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"];
 const allIntervals = ["P1", "m2", "M2", "m3", "M3", "P4", "d5", "P5", "m6", "M6", "m7", "M7"];
-const numOfFrets = 23;
+const allChoords = [new Choord("major", ["P1", "M3", "P5"]),
+                    new Choord("minor", ["P1", "m3", "P5"]),	
+                    new Choord("aug",   ["P1", "M3", "m6"]),
+                    new Choord("sus4",  ["P1", "P4", "P5"]),
+                    new Choord("sus2",  ["P1", "M2", "P5"]),
+                    new Choord("maj7",  ["P1", "M3", "P5", "M7"]),
+                    new Choord("7",     ["P1", "M3", "P5", "m7"]),
+                    new Choord("m7",    ["P1", "m3", "P5", "m7"]),
+                    new Choord("7b5",   ["P1", "m3", "d5", "m7"]),
+                    new Choord("dim",   ["P1", "m3", "d5", "M6"]),
+                    new Choord("6",     ["P1", "M3", "P5", "M6"]),
+                    new Choord("m6",    ["P1", "m3", "P5", "M6"]),
+                    new Choord("add9",  ["P1", "M2", "M3", "P5"]),
+                    new Choord("9",     ["P1", "M2", "M3", "P5", "m7"]),
+                    new Choord("maj9",  ["P1", "M2", "M3", "P5", "M7"]),
+                    new Choord("m9",    ["P1", "M2", "m3", "P5", "m7"]),
+                    new Choord("6/9",   ["P1", "M2", "M3", "P5", "M6"]),
+                    new Choord("11",    ["P1", "M2", "M3", "P4", "P5", "m7"]),
+                    new Choord("m11",   ["P1", "M2", "m3", "P4", "P5", "m7"]),
+                    new Choord("13",    ["P1", "M2", "M3",, "P5", "M6", "m7"])];
+//const allScales; // TODO
 
-var choosenRoot; 
+var choosenRoot = "C"; 
+var choosenChoord = "Major";
 var choosenIntervals = [];
 var choosenNotes = [];
 var strings = [];
-
 var tuning = ["E", "B", "G", "D", "A", "E"];
-var xCoords = [44, 100, 206, 308, 400, 486, 568, 646, 719, 788, 851, 912, 968, 1023, 1075, 1122, 1169, 1212, 1251, 1290, 1326, 1360 , 1392];
-var k = [-0.012383901, -0.00619195, -0.001547988, 0.004643963, 0.01006192, 0.013931889];
-var m = [42.23839009, 63.61919505, 86.15479876, 108.5356037, 129.993808, 152.6068111];
-
 
 // Define canvas
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 
+// Define coordinates for plotting the notes on the fretboard
+var xCoords = [44, 100, 206, 308, 400, 486, 568, 646, 719, 788, 851, 912, 968, 1023, 1075, 1122, 1169, 1212, 1251, 1290, 1326, 1360 , 1392];
+var k = [-0.012383901, -0.00619195, -0.001547988, 0.004643963, 0.01006192, 0.013931889];
+var m = [42.23839009, 63.61919505, 86.15479876, 108.5356037, 129.993808, 152.6068111];
+
+// Main update loop to set notes and intervals correctly
 function Update() {
     ctx.clearRect(0, 0, c.width, c.height);
     choosenIntervals.length = 0;
     choosenNotes.length = 0;
 
+    //Update settings from user
     let e = document.getElementById("Root");
     choosenRoot = e.options[e.selectedIndex].value;
-
     e = document.getElementById("Format");
     let format = e.options[e.selectedIndex].value;
-    
-    if (format === "Intervals")
+    e = document.getElementById("Choord");
+    choosenChoord = e.options[e.selectedIndex].value;
+
+    // Fill array strings with notes, since the strings can be tuned differently.
+    tuning.forEach(elementString => {
+        strings.push(BuildString(elementString));
+    });
+
+    if (format === "Root")
         DrawIntervals();
-    else if (format === "Chords")
-        DrawChoord();
-    else if (format === "Root")
-        DrawRoots();
+    else if (format === "Intervals") {
+        allIntervals.forEach(element => {
+            try {
+                if (document.getElementById(element).checked) 
+                    choosenIntervals.push(element);
+            } catch (error) {
+                
+            }
+        });
+    
+        choosenIntervals.forEach(element => {
+            choosenNotes.push(FindNoteOfInterval(choosenRoot, element));
+        });
+
+        DrawIntervals();
+    }
+    else if (format === "Chords") {
+        allChoords.forEach(element => {
+            if (element.getName() === choosenChoord)
+                element.getIntervals().forEach(element => {
+                    choosenNotes.push(FindNoteOfInterval(choosenRoot, element));
+                });
+        });
+        
+        DrawIntervals();
+    }
 }
 
+// Clear canvas
 function Clear() {
     ctx.clearRect(0, 0, c.width, c.height);
 }
 
-function DrawRoots() {
-    tuning.forEach(elementString => {
-        strings.push(BuildString(elementString));
-    });
-
-    for (var i = 0; i<strings.length; i++) {
-        for (var j = 0; j<strings[i].length; j++){
-            if (strings[i][j] === choosenRoot)
-                DrawNote(ctx, i, j, strings[i][j], true);
-        }
-    }
-}
-
+// Method to draw choosen intervals on fretboard
 function DrawIntervals() {
-    allIntervals.forEach(element => {
-        try {
-            if (document.getElementById(element).checked) 
-                choosenIntervals.push(element);
-        } catch (error) {
-            
-        }
-    });
-
-    choosenIntervals.forEach(element => {
-        choosenNotes.push(FindNoteOfInterval(choosenRoot, element));
-    });
-
-    tuning.forEach(elementString => {
-        strings.push(BuildString(elementString));
-    });
-
-    for (var i = 0; i<strings.length; i++) {
-        for (var j = 0; j<strings[i].length; j++){
-            if (strings[i][j] === choosenRoot)
-                DrawNote(ctx, i, j, strings[i][j], true);
-            else if (choosenNotes.includes(strings[i][j]))
-                DrawNote(ctx, i, j, strings[i][j], false);
-        }
-    }
-}
-
-
-function DrawChoord() {
-    e = document.getElementById("Choord");
-    let choosenChoord = e.options[e.selectedIndex].value;
-
-    allChoords.forEach(element => {
-        if (element.getName() === choosenChoord)
-            element.getIntervals().forEach(element => {
-                choosenNotes.push(FindNoteOfInterval(choosenRoot, element));
-            });
-    });
-
-    tuning.forEach(elementString => {
-        strings.push(BuildString(elementString));
-    });
-
     for (var i = 0; i<strings.length; i++) {
         for (var j = 0; j<strings[i].length; j++){
             if (strings[i][j] === choosenRoot)
